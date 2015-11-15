a9b73fe6b067e15949cf8f30f59d277126a058fd
ViewFromTheBox <jacob.martella@att.net>
2015-11-15 14:17:04 -0600

    Added Background color for "Breaking News" section

---
+++ jm-breaking-news.php
@@ -0,0 +1,122 @@
+<?php 
+/*
+* Plugin Name: JM Breaking News
+* Plugin URI: http://www.jacobmartella.com/jm-breaking-news/
+* Description: Displays a breaking news banner, similar to that of CNN, with custom text and link on the webpage via a custom post type.
+* Version: 1.3.1
+* Author: Jacob Martella
+* Author URI: http://www.jacobmartella.com
+* License: GPLv3
+*/
+
+/**
+* Set up the plugin when the user activates the plugin. Adds the breaking news custom post type the text domain for translations.
+*/
+
+$jm_breaking_news_plugin_path = plugin_dir_path( __FILE__ );
+define('JM_BREAKING_NEWS_PATH', $jm_breaking_news_plugin_path);
+
+//* Load the page for the custom post type
+include_once(JM_BREAKING_NEWS_PATH . 'admin/breaking-news-cpt.php');
+
+//* Load the custom fields for the breaking news
+include_once(JM_BREAKING_NEWS_PATH . 'admin/breaking-news-fields.php');
+
+//* Load the contextual help for the breaking news
+include_once(JM_BREAKING_NEWS_PATH . 'admin/breaking-news-contextual-help.php');
+
+//* Load the text domain
+load_plugin_textdomain('jm-breaking-news', false, basename( dirname( __FILE__ ) ) . '/languages' );
+
+/**
+* Loads and registers the stylesheet for the breaking news banner
+*/
+function jm_breaking_news_style() {
+	wp_register_style('jm-breaking-news-style', plugin_dir_url(__FILE__) . '/css/breaking-news-style.css');
+	wp_enqueue_style('jm-breaking-news-style');
+	wp_register_style('lato', '//fonts.googleapis.com/css?family=Lato:100,300,400,700');
+  	wp_enqueue_style( 'lato');
+  	wp_register_style('oswald', '//fonts.googleapis.com/css?family=Oswald:400,700,300');
+  	wp_enqueue_style( 'oswald');
+}
+add_action('wp_enqueue_scripts', 'jm_breaking_news_style');
+
+/**
+* Loads the script for the breaking news custom post type
+*/
+function jm_breaking_news_admin_scripts() {
+	global $typenow;
+	if ($typenow == 'jm_breaking_news') {
+		wp_enqueue_script( 'wp-color-picker');
+		wp_enqueue_style( 'wp-color-picker' );
+		wp_enqueue_script('jm_breaking_news_admin_script', plugin_dir_url(__FILE__) . 'js/breaking-news-show-hide-fields.js');
+		wp_enqueue_style('jm_breaking_news_admin_styles', plugin_dir_url(__FILE__) . 'css/breaking-news-admin-style.css');
+	}
+}
+add_action('admin_enqueue_scripts', 'jm_breaking_news_admin_scripts');
+
+/**
+* Returns the HTML for the breaking news banner
+*
+* Add '<?php echo jm_breaking_news(); ?>' anywhere in your theme to display the breaking news.
+*
+* @return string, HTML for the banner
+*/
+function jm_breaking_news() {
+	$html = '';
+	$jm_breaking_news_args = array(
+		'post_type' => 'jm_breaking_news',
+		'posts_per_page' => 1,
+	);
+	$jm_breaking_news = new WP_Query($jm_breaking_news_args);
+	if ($jm_breaking_news->have_posts()) : while ($jm_breaking_news->have_posts()) : $jm_breaking_news->the_post();
+		$current_time = strtotime(current_time('mysql'));
+		$post_time = strtotime(get_the_date('r'));
+		$difference = ($current_time - $post_time)/(60 * 60);
+		$limit = get_post_meta(get_the_ID(), 'jm_breaking_news_limit', true);
+		if (get_post_meta(get_the_ID(), 'jm_breaking_news_target', true) == 1) {
+			$target = 'target="_blank"';
+		} else {
+			$target = '';
+		}
+		if (get_post_meta(get_the_ID(), 'jm_breaking_news_in_ex', true) == 1) {
+			$link = get_post_meta(get_the_ID(), 'jm_breaking_news_internal_link', true);
+		} else {
+			$link = get_post_meta(get_the_ID(), 'jm_breaking_news_link', true);
+		}
+		if (get_post_meta(get_the_ID(), 'jm_breaking_news_color', true)) {
+			$style = 'style="background-color:' . get_post_meta(get_the_ID(), 'jm_breaking_news_color', true) . ';"';
+		} else {
+			$style = '';
+		}
+		if ($difference < $limit) {
+			$html .= '<section class="breaking-news-box">';
+			$html .= '<div class="breaking-news-left" ' . $style . '>';
+			$html .= '<h2 class="breaking-news-left-h2">' . __('Breaking News', 'jm-breaking-news') . '</h2>';
+			$html .= '</div>';
+			$html .= '<div class="breaking-news-right">';
+			$html .= '<h2 class="breaking-news-right-h2"><a href="' . $link . '" ' . $target .'>' . get_the_title() . '</a></h2>';
+			$html .= '</div>';
+			$html .= '</section>';
+		}
+	endwhile; endif;
+	wp_reset_query();
+
+	return $html;
+}
+
+//* Breaking News Custom RSS Feed
+add_action( 'wp_head', 'jm_breaking_news_feed' );
+ 
+function jm_breaking_news_feed() {
+    $post_types = array('jm_breaking_news');
+    foreach( $post_types as $post_type ) {
+        $feed = get_post_type_archive_feed_link( $post_type );
+        if ( $feed === '' || !is_string( $feed ) ) {
+            $feed =  get_bloginfo( 'rss2_url' ) . "?post_type=$post_type";
+        }
+        printf(__('<link rel="%1$s" type="%2$s" href="%3$s" />'),"alternate","application/rss+xml",$feed);
+    }
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,56 @@
+=== JM Breaking News ===
+Contributors: ArenaPigskin
+Tags: breaking news, banner
+Requires at least: 4.0
+Tested up to: 4.3
+Stable tag: 1.3.1
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl.html
+
+Display a breaking news banner for a selected amount of time anywhere on your site.
+
+== Description ==
+The JM Breaking News Plugin allows you to display a breaking news banner with custom text and a custom link for as long as you want anywhere on the site.
+
+== Installation ==
+To have WordPress download and install the plugin, go to the WordPress dashboard, click "Plugins", click "Add New" and search for "JM Breaking News" and click "Install." To manually download and install the plugin, download the plugin, either from the plugin site or the plugin's page from the Plugin Directory. The in the WordPress dashboard, go to "Plugins" then "Add New" then "Upload Plugin" and upload the plugin's zip file.
+
+Once the plugin has been uploaded and activated, add the following code into your theme where you want it to be shown.
+`<?php
+if (function_exists('jm_breaking_news')) {
+	echo jm_breaking_news();
+} ?>`
+
+The banner has a width of 100%, so it will fit into any size container; however, the best use for the banner is to have it in the header.
+
+== Screenshots ==
+1. The editor screen to add breaking news.
+2. The breaking news banner in action on the web site.
+
+== Changelog ==
+
+= 1.3.1 =
+- Removed 'http:' from the functions to enqueue the Google fonts
+
+= 1.3 =
+- Tested to work with WordPress 4.3
+- Fixed vertical margin for "Breaking News" text to even it out.
+- Added a line-height of 1.2 for text on the right side to even it out.
+
+= 1.2.1 =
+- Fixes: Issue with the JavaScript file for the breaking news post type admin area not being included in the plugin.
+
+= 1.2 =
+- Added: Option for users to select whether the link will be internal or external.
+- Added: A select option of posts for the user to select if it's an internal link.
+
+= 1.1 =
+- Added: default value of 1 for the time limit.
+- Added: wp_reset_query at the end of the jm_breaking_news function to keep the banner from interfering with the page query.
+- Changed: the left side font to Oswald.
+- Changed: the right side font to Lato.
+- Changed: the left side background to #C42B2B.
+- Changed: the right side background to #252525.
+
+= 1.0 =
+- Initial Release
